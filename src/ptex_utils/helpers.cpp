48c7cf8554a77c10547add7434a740e827c35c98
Leonid Onokhov <sopvop@gmail.com>
2015-03-19 16:16:50 +0300

    Fix strbasename bug

--- src/ptex_utils/helpers.cpp
+++ src/ptex_utils/helpers.cpp
@@ -8,9 +8,9 @@ std::string strbasename(const char* s)
         return ".";
     size_t i = std::strlen(s)-1;
     size_t end = 0;
-    for (; i && s[i] == '/'; i--){}
-    end = i+1;
-    for (; i && s[i-1] != '/'; i--){}
+    for (; i && s[i] == '/'; i--);
+    end = *(s+i) ? i+1 : i;
+    for (; i && s[i-1] != '/'; i--);
 
-    return std::string(s+i, end);
+    return std::string(s+i, end-i);
 }
