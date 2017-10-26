caaf033de45c773ce8ad8511cde9b92565693c79
Leonid Onokhov <sopvop@gmail.com>
2017-10-26 22:49:13 +0300

    Fix shared libs and update nix

--- default.nix
+++ default.nix
@@ -1,17 +1,18 @@
-{ mkDerivation, cmakeCleanSource, cmake
-, boost_static, ptex_static, zlib_static }:
-mkDerivation {
+{ stdenv, cmakeCleanSource, cmake
+, boost, ptex, zlib
+ }:
+stdenv.mkDerivation {
   name="ptextools";
   src= cmakeCleanSource ./.;
   nativeBuildInputs = [ cmake ];
-  propagatedBuildInputs = [ boost_static ptex_static zlib_static ];
+  propagatedBuildInputs = [ boost ptex zlib ];
   cmakeFlags = [
-    "-DBUILD_SHARED_LIBS:BOOL=OFF"
+    "-DBUILD_SHARED_LIBS:BOOL=ON"
     "-DBUILD_PYTHON_MODULE:BOOL=OFF"
-    "-DBOOST_ROOT=${boost_static}"
-    "-DPTEX_LOCATION=${ptex_static}"
-    "-DCMAKE_LIBRARY_PATH:PATH=${zlib_static}/lib"
-    "-DCMAKE_INCLUDE_PATH:PATH=${zlib_static}/include"
+    "-DBOOST_ROOT=${boost}"
+    "-DPTEX_LOCATION=${ptex}"
+    "-DCMAKE_LIBRARY_PATH:PATH=${zlib}/lib"
+    "-DCMAKE_INCLUDE_PATH:PATH=${zlib}/include"
     "-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON"
     "-DCMAKE_POLICY_DEFAULT_CMP0063=NEW"
   ];
